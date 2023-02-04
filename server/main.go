6cbc7b7f1505a7a5b866466a56be4191139b767d
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-02-04 10:34:30 +0800

    refactor

--- server/main.go
+++ server/main.go
@@ -4,16 +4,16 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/bjornpagen/e2e-marketing-monorepo/server/hashlookup"
+	"github.com/bjornpagen/e2e-marketing-monorepo/server/idlookup"
 )
 
 func main() {
-	hashToEmailMap := map[string]string{
+	idToEmailMap := map[string]string{
 		"b1a6b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4b0c4": "bjorn.pagen@example.com",
 	}
 
-	hashlookupClient := hashlookup.New(hashToEmailMap)
-	http.HandleFunc("/hashlookup", hashlookupClient.HashLookupHandler)
+	idlookupClient := idlookup.New(idToEmailMap)
+	http.HandleFunc("/idlookup", idlookupClient.IdLookupHandler)
 
 	log.Println("Listening on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {

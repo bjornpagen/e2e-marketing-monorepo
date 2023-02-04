b23cca39c5e3f95545bfd8d1f2ddb9dfa6b130fb
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-02-04 12:11:04 +0800

    correct resource url

--- client/modules/getEmailById.js
+++ client/modules/getEmailById.js
@@ -1,6 +1,6 @@
 // endpoint is the domain name of the endpoint, no https:// or trailing slash
 const getEmailById = async (endpoint, id) => {
-  const response = await fetch(`https://${endpoint}/idlookup`, {
+  const response = await fetch(`https://${endpoint}/lookup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
